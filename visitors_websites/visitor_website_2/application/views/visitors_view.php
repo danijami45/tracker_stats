7cf41d749ca91776823525c2784ca26b22ca9367
danish <syeddanishjamil45@gmail.com>
2024-11-10 16:21:44 +0000

    visitor website project folders added for testing if needed.

---
+++ visitors_websites/visitor_website_2/application/views/visitors_view.php
@@ -0,0 +1,11 @@
+<html>
+    <head></head>
+    <body>
+
+        <div>Test visitor 2.</div>
+
+        <!-- Scripts loading -->
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/visitor_tracker.js"></script>
+    </body>
+</html>
